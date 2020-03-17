de7165576053a6f7d79b4ad1dcb6b07827087daa
Linton Junior <lintonjr@hotmail.com>
2020-03-17 11:08:51 -0400

    Controle de erros nas respostas e decremento de vidas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
 
@@ -9,33 +9,48 @@ import { FRASES } from './frases-mock'
 })
 export class PainelComponent implements OnInit {
 
-  public frases: Array<Frase> = FRASES
-  public instrucao: string = 'Traduza a frase!'
-  public resposta: string
+  public frases: Array<Frase> = FRASES;
+  public instrucao: string = 'Traduza a frase!';
+  public resposta: string = '';
 
-  public rodada: number = 0
-  public rodadaFrase: Frase
-  public progresso: number = 0
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+  public progresso: number = 0;
+
+  public tentativas: number = 3;
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
+    this.atualizaRodada();
   }
 
   ngOnInit(): void {
   }
 
   public atualizaResposta(resposta: Event): void {
-    this.resposta = (<HTMLInputElement>resposta.target).value
+    this.resposta = (resposta.target as HTMLInputElement).value;
   }
 
   public verificarResposta(): void {
+    // tslint:disable-next-line: triple-equals
     if (this.rodadaFrase.frasePtBr == this.resposta) {
-      alert('Traducao correta')
-      this.rodada++
-      this.progresso = this.progresso + (100 / this.frases.length)
-      this.rodadaFrase = this.frases[this.rodada]
+      this.rodada++;
+      this.progresso = this.progresso + (100 / this.frases.length);
+      if(this.rodada == 4) {
+        this.encerrarJogo.emit('vitoria');
+      }
+      this.atualizaRodada();
     } else {
-      alert('Traducao errada')
+      this.tentativas--;
+      if(this.tentativas === -1) {
+        this.encerrarJogo.emit('derrota');
+      }
     }
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
   }
 }
